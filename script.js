c3b1d9db689b42e8e336f3917a10efdd0c3f3cac
Matias Balestra <93502743+MatiasBalestra@users.noreply.github.com>
2022-01-17 22:58:01 -0300

    #ChallengeOneLogicaLatam
    #ChallengeOneLogicaLatam

---
+++ script.js
@@ -0,0 +1,22 @@
+function btnCopiar(){
+    const copyText = document.getElementById("campo2");
+    copyText.select();
+    document.execCommand("Copy");
+}
+
+function btnPegar(){
+    document.getElementById("campo1").value = document.getElementById("campo2").value;
+}
+
+function btnEncriptar(){
+        const encriptar = document.getElementById('campo1').value;
+        const encriptado = encriptar.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+        document.getElementById("campo2").value = encriptado
+    }
+
+
+function btnDesEncriptar(){
+        const desencriptar= document.getElementById ("campo1").value;
+        const desencriptado= desencriptar.replace(/ai/gi, 'a').replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+        document.getElementById("campo2").value = desencriptado
+    }
